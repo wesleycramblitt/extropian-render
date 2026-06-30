593f45850716190877c149201f43524b56c2f968
Wesley Cramblitt <wcramblitt@gmail.com>
2026-06-29 19:43:32 -0600

    Fix Mesh Vec3 -> Vec3f

--- include/exd/render/mesh.hpp
+++ include/exd/render/mesh.hpp
@@ -6,9 +6,9 @@
 namespace exd::render {
 
 struct Vertex {
-    math::Vec3 position{};
-    math::Vec3 normal{0.0f, 1.0f, 0.0f};
-    math::Vec3 uv{0.0f, 0.0f, 0.0f};
+    math::Vec3f position{};
+    math::Vec3f normal{0.0f, 1.0f, 0.0f};
+    math::Vec3f uv{0.0f, 0.0f, 0.0f};
     math::Quat tangent{1.0f, 0.0f, 0.0f, 1.0f};
     math::Quat color{0.8f, 0.8f, 0.8f, 1.0f};
 };
